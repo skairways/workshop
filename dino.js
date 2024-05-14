b459ad3740d283e9c8c6aae6ea38e4db5aa4c108
Sultonbek <kingsulton45@gmail.com>
2024-05-14 17:51:33 +0500

    collision

--- dino.js
+++ dino.js
@@ -1,52 +1,150 @@
-let board;
-let boardHeight = 250;
-let boardWidth = 750;
+// window.onload = () => new Game("./img/dino.png", "./img/dino-dead.png");
+window.onload = () =>
+  new Game(
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShTqCHlRmun5QqSr-8ayx1kGg3mbiCBCM4d_v6vQOBlePi4z79hci6K_6Hz6Vfe0trIiI&usqp=CAUhttps://art.pixilart.com/c2e4bb6cc4a4fc7.png",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLnJJy77oKznWTj9X4BpfL8x3tXARsMvC8uCWq27CUOw&s"
+  );
 
-let dinoWidth = 88;
-let dinoHeight = 94;
+class Game {
+  board;
+  boardHeight = 250;
+  boardWidth = 750;
 
-let dinoX = 50;
-let dinoY = boardHeight - dinoHeight;
+  dinoWidth = 88;
+  dinoHeight = 94;
 
-let dino = {
-  x: dinoX,
-  y: dinoY,
-  width: dinoWidth,
-  height: dinoHeight,
-};
+  dinoX = 50;
+  dinoY = this.boardHeight - this.dinoHeight;
 
-let velocityY = 0;
-let dinoImg;
-let context;
-let gravity = 0.4;
-
-window.onload = function () {
-  board = document.getElementById("board");
-  board.height = boardHeight;
-  board.width = boardWidth;
-  context = board.getContext("2d");
-
-  dinoImg = new Image();
-  dinoImg.src = "./img/dino.png";
-  dinoImg.onload = function () {
-    context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+  dino = {
+    x: this.dinoX,
+    y: this.dinoY,
+    width: this.dinoWidth,
+    height: this.dinoHeight,
   };
 
-  requestAnimationFrame(update);
-  document.addEventListener("keydown", moveDino);
-};
+  velocityY = 0;
+  velocityX = -8;
+  dinoImg;
+  context;
+  gravity = 0.4;
 
-function moveDino(e) {
-  if ((e.code == "Space" || e.code == "ArrowUp") && dino.y == dinoY) {
-    velocityY = -10;
+  cactusHeight = 70;
+  cactusX = 700;
+  cactusY = this.boardHeight - this.cactusHeight;
+  cactusArray = [];
+
+  gameOver = false;
+
+  collisionItemsList = [
+    { img: "./img/cactus3.png", width: 102, occur: 10 },
+    { img: "./img/cactus2.png", width: 69, occur: 20 },
+    { img: "./img/cactus1.png", width: 34, occur: 100 },
+  ];
+
+  constructor(userImg, deadImg) {
+    this.board = document.getElementById("board");
+    this.board.height = this.boardHeight;
+    this.board.width = this.boardWidth;
+    this.context = board.getContext("2d");
+    this.dinoImg = Game.createImage(userImg);
+    this.deadImg = deadImg;
+    this.dinoImg.onload = () =>
+      this.context.drawImage(
+        this.dinoImg,
+        this.dino.x,
+        this.dino.y,
+        this.dino.width,
+        this.dino.height
+      );
+
+    requestAnimationFrame(this.update.bind(this));
+    document.addEventListener("keydown", this.move.bind(this));
+    setInterval(this.placeItem.bind(this), 1000);
   }
-}
 
-function update() {
-  requestAnimationFrame(update);
-  context.clearRect(0, 0, board.width, board.height);
-  //dino
-  velocityY += gravity;
-  dino.y = Math.min(dino.y + velocityY, dinoY);
-  context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+  static createImage(url) {
+    let img = new Image();
+    img.src = url;
+    return img;
+  }
+
+  move(e) {
+    if (
+      (e.code == "Space" || e.code == "ArrowUp") &&
+      this.dino.y == this.dinoY
+    ) {
+      this.velocityY = -10;
+    }
+  }
+
+  update() {
+    if (this.gameOver) return;
+    requestAnimationFrame(this.update.bind(this));
+    this.context.clearRect(0, 0, this.board.width, this.board.height);
+    //dino
+    this.velocityY += this.gravity;
+    this.dino.y = Math.min(this.dino.y + this.velocityY, this.dinoY);
+    this.context.drawImage(
+      this.dinoImg,
+      this.dino.x,
+      this.dino.y,
+      this.dino.width,
+      this.dino.height
+    );
+
+    //cactus
+    for (let i = 0; i < this.cactusArray.length; i++) {
+      let cactus = this.cactusArray[i];
+      cactus.x += this.velocityX;
+      this.context.drawImage(
+        cactus.img,
+        cactus.x,
+        cactus.y,
+        cactus.width,
+        cactus.height
+      );
+
+      if (this.detectCollision(this.dino, cactus)) {
+        this.gameOver = true;
+        this.dinoImg.src = this.deadImg;
+        this.dinoImg.onload = () =>
+          this.context.drawImage(
+            this.dinoImg,
+            this.dino.x,
+            this.dino.y,
+            this.dino.width,
+            this.dino.height
+          );
+      }
+    }
+  }
+
+  placeItem() {
+    const random = Math.random();
+    const item = this.collisionItemsList.find(
+      (item) => item.occur > random * 100
+    );
+    let cactus = {
+      x: this.cactusX,
+      y: this.cactusY,
+      height: this.cactusHeight,
+      ...item,
+    };
+    cactus.img = Game.createImage(item.img);
+
+    this.cactusArray.push(cactus);
+    if (this.cactusArray.length > 5) {
+      this.cactusArray.shift();
+    }
+  }
+
+  detectCollision(a, b) {
+    return (
+      a.x + a.width > b.x &&
+      a.x < b.x + b.width &&
+      a.y < b.y + b.height &&
+      a.y + a.height > b.y
+    );
+  }
 }
