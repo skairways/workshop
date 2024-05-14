a1dbaae1448c0d7eef41f8c01536cb78e2ec2e7c
Sultonbek <kingsulton45@gmail.com>
2024-05-14 19:32:44 +0500

    refactor

--- dino.js
+++ dino.js
@@ -1,61 +1,81 @@
-// window.onload = () => new Game("./img/dino.png", "./img/dino-dead.png");
+const board = {
+  height: 250,
+  width: 750,
+  element: "board",
+};
+
+const player = {
+  width: 88,
+  height: 94,
+  x: 50,
+};
+
+const obstacle = {
+  height: 70,
+  x: 700,
+};
+
+const obstacleItemList = [
+  { img: "./img/cactus3.png", width: 102, occur: 10 },
+  { img: "./img/cactus2.png", width: 69, occur: 20 },
+  { img: "./img/cactus1.png", width: 34, occur: 100 },
+];
+
 window.onload = () =>
   new Game(
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShTqCHlRmun5QqSr-8ayx1kGg3mbiCBCM4d_v6vQOBlePi4z79hci6K_6Hz6Vfe0trIiI&usqp=CAUhttps://art.pixilart.com/c2e4bb6cc4a4fc7.png",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLnJJy77oKznWTj9X4BpfL8x3tXARsMvC8uCWq27CUOw&s"
+    // player-img
+    "./img/dino.png",
+    // player-dead-img
+    "./img/dino-dead.png",
+    board,
+    player,
+    obstacle,
+    obstacleItemList
   );
 
 class Game {
-  board;
-  boardHeight = 250;
-  boardWidth = 750;
-
-  dinoWidth = 88;
-  dinoHeight = 94;
-
-  dinoX = 50;
-  dinoY = this.boardHeight - this.dinoHeight;
-
-  dino = {
-    x: this.dinoX,
-    y: this.dinoY,
-    width: this.dinoWidth,
-    height: this.dinoHeight,
-  };
-
   velocityY = 0;
-  velocityX = -8;
-  dinoImg;
-  context;
+  velocityX = -10;
   gravity = 0.4;
-
-  cactusHeight = 70;
-  cactusX = 700;
-  cactusY = this.boardHeight - this.cactusHeight;
-  cactusArray = [];
-
+  obstacleList = [];
   gameOver = false;
 
-  collisionItemsList = [
-    { img: "./img/cactus3.png", width: 102, occur: 10 },
-    { img: "./img/cactus2.png", width: 69, occur: 20 },
-    { img: "./img/cactus1.png", width: 34, occur: 100 },
-  ];
+  constructor(
+    playerImg,
+    deadImg,
+    boardProps,
+    player,
+    obstacle,
+    obstacleItemList
+  ) {
+    // board
+    this.board = document.getElementById(boardProps.element);
+    this.board.height = boardProps.height;
+    this.board.width = boardProps.width;
+    // player
+    this.playerX = player.x;
+    this.playerY = boardProps.height - player.height;
+    this.player = {
+      x: this.playerX,
+      y: this.playerY,
+      width: player.width,
+      height: player.height,
+    };
 
-  constructor(userImg, deadImg) {
-    this.board = document.getElementById("board");
-    this.board.height = this.boardHeight;
-    this.board.width = this.boardWidth;
-    this.context = board.getContext("2d");
-    this.dinoImg = Game.createImage(userImg);
+    this.obstacle = obstacle;
+    this.obstacle.y = boardProps.height - obstacle.height;
+    this.obstacleItemList = obstacleItemList;
+
+    this.context = this.board.getContext("2d");
+    this.playerImg = Game.createImage(playerImg);
     this.deadImg = deadImg;
-    this.dinoImg.onload = () =>
+    this.playerImg.onload = () =>
       this.context.drawImage(
-        this.dinoImg,
-        this.dino.x,
-        this.dino.y,
-        this.dino.width,
-        this.dino.height
+        this.playerImg,
+        this.player.x,
+        this.player.y,
+        this.player.width,
+        this.player.height
       );
 
     requestAnimationFrame(this.update.bind(this));
@@ -72,7 +92,7 @@ class Game {
   move(e) {
     if (
       (e.code == "Space" || e.code == "ArrowUp") &&
-      this.dino.y == this.dinoY
+      this.player.y == this.playerY
     ) {
       this.velocityY = -10;
     }
@@ -82,39 +102,38 @@ class Game {
     if (this.gameOver) return;
     requestAnimationFrame(this.update.bind(this));
     this.context.clearRect(0, 0, this.board.width, this.board.height);
-    //dino
     this.velocityY += this.gravity;
-    this.dino.y = Math.min(this.dino.y + this.velocityY, this.dinoY);
+    this.player.y = Math.min(this.player.y + this.velocityY, this.playerY);
     this.context.drawImage(
-      this.dinoImg,
-      this.dino.x,
-      this.dino.y,
-      this.dino.width,
-      this.dino.height
+      this.playerImg,
+      this.player.x,
+      this.player.y,
+      this.player.width,
+      this.player.height
     );
 
     //cactus
-    for (let i = 0; i < this.cactusArray.length; i++) {
-      let cactus = this.cactusArray[i];
-      cactus.x += this.velocityX;
+    for (let i = 0; i < this.obstacleList.length; i++) {
+      let newObstacle = this.obstacleList[i];
+      newObstacle.x += this.velocityX;
       this.context.drawImage(
-        cactus.img,
-        cactus.x,
-        cactus.y,
-        cactus.width,
-        cactus.height
+        newObstacle.img,
+        newObstacle.x,
+        newObstacle.y,
+        newObstacle.width,
+        newObstacle.height
       );
 
-      if (this.detectCollision(this.dino, cactus)) {
+      if (this.detectCollision(this.player, newObstacle)) {
         this.gameOver = true;
-        this.dinoImg.src = this.deadImg;
-        this.dinoImg.onload = () =>
+        this.playerImg.src = this.deadImg;
+        this.playerImg.onload = () =>
           this.context.drawImage(
-            this.dinoImg,
-            this.dino.x,
-            this.dino.y,
-            this.dino.width,
-            this.dino.height
+            this.playerImg,
+            this.player.x,
+            this.player.y,
+            this.player.width,
+            this.player.height
           );
       }
     }
@@ -122,20 +141,20 @@ class Game {
 
   placeItem() {
     const random = Math.random();
-    const item = this.collisionItemsList.find(
+    const item = this.obstacleItemList.find(
       (item) => item.occur > random * 100
     );
-    let cactus = {
-      x: this.cactusX,
-      y: this.cactusY,
-      height: this.cactusHeight,
+    let newObstacle = {
+      x: this.obstacle.x,
+      y: this.obstacle.y,
+      height: this.obstacle.height,
       ...item,
     };
-    cactus.img = Game.createImage(item.img);
+    newObstacle.img = Game.createImage(item.img);
 
-    this.cactusArray.push(cactus);
-    if (this.cactusArray.length > 5) {
-      this.cactusArray.shift();
+    this.obstacleList.push(newObstacle);
+    if (this.obstacleList.length > 5) {
+      this.obstacleList.shift();
     }
   }
 
